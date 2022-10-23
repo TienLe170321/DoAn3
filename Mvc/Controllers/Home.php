998b1cb1e56716ecc1cebf336c64aa5fc328c0a5
TienLe170321 <lvtien1900485@student.ctuet.edu.vn>
2022-10-23 23:36:22 +0700

    Cau hinh htaccess, xu li url

---
+++ Mvc/Controllers/Home.php
@@ -0,0 +1,10 @@
+<?php 
+	class Home{
+		function Show(){
+			echo "Home-show-Trang chu";
+		}
+		function sanpham(){
+			echo "Home-sanpham-ds sanpham";
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+	session_start();
+	require_once "./Mvc/Bridge.php";
+	$App = new App();
+?>
